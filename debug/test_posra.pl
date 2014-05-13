7098956beb03974869be2a93e870c3d313487009
Nicholas Hayden Smith <nihasmit@ucsc.edu>
2014-05-13 09:55:37 -0700

    Use strict perl test script

--- debug/test_posra.pl
+++ debug/test_posra.pl
@@ -1,10 +1,10 @@
-#!/bin/perl
+#!/usr/bin/perl
+use strict;
 open FILE, "<", "./debug/posra_tests.txt" or die $!;
 my @image_paths = <FILE>;
 close(FILE);
 my $counter = 0;
 foreach (@image_paths) {
-      #print $_;
       if (substr($_, 0, 1) ne "#") {
             print $_;
             $counter += test($_);
@@ -16,15 +16,16 @@ print "$counter / $total tests passed.";
 
 sub test {
       my $res = 0;
-      @io = split(" ", $_[0]);
+      my @io = split(" ", $_[0]);
       $io[0] =~s/ /\\ /g;
-      $input = "./src/osra " . "\"$io[0]\"" . " |";
+      my $input = "./src/osra " . "\"$io[0]\"" . " |";
       open POSRA, $input or die $!;
-      @output = <POSRA>;
+      my @output = <POSRA>;
+      close(POSRA);
       foreach (@output) {
             print "\t$_";
       }
-      $i = ((scalar @output) < 2) ? 0 : 1;
+      my $i = ((scalar @output) < 2) ? 0 : 1;
       chomp($output[$i]);
       chomp($io[1]);
       if ($output[$i] eq $io[1]) {
@@ -33,6 +34,5 @@ sub test {
       } else {
             print "\t[ \e[31mFAIL\e[0m ]\n";
       }
-      close(POSRA);
       return $res;
 }
